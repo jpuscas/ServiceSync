4bcaa92f1ccfa7cf60e4deeb4ec7d67f53477964
jnorman42 <phantompiano15@gmail.com>
2026-02-26 13:20:32 -0500

    user database creation

--- CIS376Project/main.py
+++ CIS376Project/main.py
@@ -1,15 +1,30 @@
 import sqlite3
-import bcrypt
 
-conn = sqlite3.connect('project.db')
-cursor = conn.cursor()
-
-
-cursor.execute("CREATE TABLE IF NOT EXISTS test (id INTEGER)")
+db = sqlite3.connect('project.db')
+cursor = db.cursor()
 
 
-conn.commit()
-conn.close()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phone TEXT,
+    
+    password TEXT NOT NULL,
+    
+    role TEXT NOT NULL DEFAULT 'member',
+    
+    is_verified INTEGER NOT NULL DEFAULT 0,
+    verification_code TEXT,
+    
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+''')
+
+db.commit()
+db.close()
 
 
 
